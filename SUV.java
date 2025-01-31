1b786c47aa77c826b4937b61e03f566024629776
csengelhart <cs.engelhartmb@gmail.com>
2025-01-31 02:36:38 -0600

    Added documentation to the SUV class

--- SUV.java
+++ SUV.java
@@ -1,5 +1,15 @@
-public class SUV extends Vehicle {
+/**
+ * SUV is a child class of Vehicle, it calls Vehicle class's constructor
+ * with the string "SUV" , which then sets an SUV object's vehicle_type to "SUV"
+ *
+ */
 
+public class SUV extends Vehicle
+{
+    /**
+     * SUV's constructor calls upon its parent class Vehicle's constructor.
+     * SUV's constructor passes the String "SUV" to Vehicle's constructor.
+     */
     public SUV()
     {
         super("SUV");
