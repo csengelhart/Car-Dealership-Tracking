53961573a1c6ef42db3439b60a7f52df4a13d8bc
csengelhart <cs.engelhartmb@gmail.com>
2025-01-31 02:30:42 -0600

    Added the rest of Vehicle's children classes Sedan, Pickup and Sports_Car.

---
+++ Sedan.java
@@ -0,0 +1,17 @@
+/**
+ * Sedan is a child class of Vehicle, it calls Vehicle class's constructor
+ * with the string "Sedan" , which then sets a Sedan object's vehicle_type to "Sedan"
+ *
+ */
+
+public class Sedan extends Vehicle
+{
+    /**
+     * Sedan's constructor calls upon its parent class Vehicle's constructor.
+     * Sedan's constructor passes the String "Sedan" to Vehicle's constructor.
+     */
+    Sedan()
+    {
+        super("Sedan");
+    }
+}
