44d94f731c6c025e63d949cb1ac57215b34e8d42
Dylan Browne <pp8436bl@go.minnstate.edu>
2025-02-02 19:47:46 -0600

    Saveing changes before switching to main

--- src/main/java/JSONIO.java
+++ src/main/java/JSONIO.java
@@ -14,10 +14,31 @@ public class JSONIO
      * @param fileName
      * @param mode
      */
-    public JSONIO(String fileName, char mode)
+    public JSONIO(String fileName, char mode) throws ReadWriteAppendException
     {
+        if (getMode(mode) == 'x') {
+            String message = "Mode (" + mode +
+                    ") is not in {'r', 'R', 'w', 'W', 'a', 'A'}";
+            throw new ReadWriteAppendException(message);
+        }
 
+    }
 
+    /**
+     * Takes a char and converts it to
+     *
+     * @param mode
+     * @return
+     */
+    private char getMode(char mode) {
+        mode = Character.toLowerCase(mode);
+        char[] valid_modes = {'r', 'w', 'a'};
+        for (char mode_lower : valid_modes) {
+            if (mode == mode_lower) {
+                return mode;
+            }
+        }
+        return 'x';
     }
 
     // for testing purposes
@@ -25,4 +46,13 @@ public class JSONIO
         System.out.println("Hello World");
     }
 
+    public int write(String[][] data) throws ReadWriteAppendException {
+        int added = 0;
+        if (mode != 'w') {
+            throw new ReadWriteAppendException();
+        }
+
+
+        return added;
+    }
 }
---
+++ src/main/java/ReadWriteAppendException.java
@@ -0,0 +1,13 @@
+/**
+ *
+ * @author Dylan Browne
+ */
+public class ReadWriteAppendException extends Exception {
+    public ReadWriteAppendException() {
+        super();
+    }
+
+    public ReadWriteAppendException(String message) {
+        super(message);
+    }
+}
