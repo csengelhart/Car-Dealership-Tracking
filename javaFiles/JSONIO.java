e01d1866395db2324793d17bf1d1827aac44fefa
csengelhart <cs.engelhartmb@gmail.com>
2025-02-05 17:08:38 -0600

    Added .gitignore

--- javaFiles/JSONIO.java
+++ javaFiles/JSONIO.java
@@ -5,7 +5,14 @@ import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
+import javax.swing.*;
+import javax.swing.filechooser.FileNameExtensionFilter;
+import java.awt.*;
 import java.io.*;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Objects;
+
 
 /**
  * A class that reads and writes to JSON files
@@ -102,8 +109,11 @@ public class JSONIO
             for (int j = 0; j < keyOrder.length; j++) {
                 String key = keyOrder[j];
                 Object dataPoint = jObj.get(key);
-                if (dataPoint instanceof Long) {
-                    output[i][j] = ( (Long) dataPoint).toString();
+                if (key.equals("acquisition_date")) { //acquisition date is in Epoch format convert to dd-mm-yyyy
+                    output[i][j] = convertMilisec_to_date((Long) dataPoint);
+
+                } else if (key.equals("price")) {
+                    output[i][j] = String.valueOf(dataPoint);
                 } else {
                     output[i][j] = (String) dataPoint;
                 }
@@ -135,9 +145,6 @@ public class JSONIO
                 JSONObject jObj = new JSONObject();
                 for (int i = 0; i < keyOrder.length; i++) {
                     Object dataPoint = carData[i];
-                    if (i >= keyOrder.length - 2) {
-                        dataPoint = Long.parseLong((String) dataPoint);
-                    }
                     jObj.put(keyOrder[i], dataPoint);
                 }
                 jArray.add(jObj);
@@ -159,17 +166,67 @@ public class JSONIO
         return added;
     }
 
+
+    /**
+     * Opens a file chooser dialog to allow the user to select a JSON file.
+     * The file chooser will start in the current user's working directory
+     * and will filter files to only show those with a ".json" extension.
+     *
+     * @return The selected JSON file if the user selects a file and confirms the dialog,
+     *         or null if the user cancels or closes the dialog without selecting a file.
+     *
+     * @author Christopher Engelhart
+     */
+    public static File selectJsonFile() {
+        JFileChooser fileChooser = new JFileChooser();
+        fileChooser.setCurrentDirectory(new File(System.getProperty("user.dir")));
+        fileChooser.setFileFilter(new FileNameExtensionFilter("JSON Files", new String[]{"json"}));
+        int result = fileChooser.showOpenDialog((Component)null);
+        return result == 0 ? fileChooser.getSelectedFile() : null;
+    }
+
+
+    /**
+     * Takes a long value that represents the number of milliseconds since 01Jan1970
+     * and converts to a date in the format "dd-MM-yyyy"
+     * @param acquisition_date long value that is the milliseconds since 01Jan1970
+     * @return formatted String representation of acquisition date
+     *
+     * @author Christopher Engelhart
+     */
+    public static String convertMilisec_to_date(long acquisition_date) {
+        Date date = new Date(acquisition_date);
+        SimpleDateFormat sdf = new SimpleDateFormat("MM-dd-yyyy");
+        return sdf.format(date);
+    }
+
+
     // for testing purposes
     public static void main(String[] args) {
         // TODO: Check inventory.json is supposed to be case sensitive
         // (download from d2l and compare to this inventory.json if I forget what this means)
 
         try {
-            JSONIO jReadExample = new JSONIO("Car-Dealership-Tracking/inventory.json", 'r');
-            JSONIO jWriteExample = new JSONIO("Car-Dealership-Tracking/output.json", 'w');
+            System.out.println("Select an inventory file to process");
+            String input_filePath = Objects.requireNonNull(selectJsonFile()).toString();
+            System.out.println("Create a name and choose location to save output file");
+            String output_filePath = Objects.requireNonNull(selectJsonFile()).toString();
+
+            // input file
+            File file = new File(input_filePath);
+            if (!file.exists() || file.length() == 0) {
+                throw new ReadWriteException("The file does not exist or is empty.");
+            }
+
+
+
+            JSONIO jReadExample = new JSONIO(input_filePath, 'r');
+
+            JSONIO jWriteExample = new JSONIO(output_filePath, 'w');
             String[][] read = jReadExample.read();
             if (jWriteExample.write(read) == read.length) {
                 System.out.println("Added all files.");
+
             }
         } catch (ReadWriteException e) {
             System.out.println(e.getMessage());
